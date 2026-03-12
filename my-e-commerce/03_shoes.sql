d7148bc168f91359d14cde6159a0f9febcaf16d8
alisabunnak <alisabunnak.2539@gmail.com>
2026-03-12 15:09:11 +0700

    update sql files

--- my-e-commerce/03_shoes.sql
+++ my-e-commerce/03_shoes.sql
@@ -1,22 +1,30 @@
 INSERT INTO shoes (brand, model, size, color, rental_price, status, category_id)
 VALUES
-('Nike','Air Max',40,'Black',150,'available',1),
-('Nike','Air Max',41,'White',150,'available',1),
-('Adidas','Ultraboost',39,'Black',160,'available',2),
-('Adidas','Ultraboost',40,'White',160,'available',2),
-('Puma','Runner',38,'Pink',120,'available',2),
-('Puma','Runner',39,'Black',120,'available',2),
-('Converse','Chuck Taylor',40,'Black',100,'available',1),
-('Converse','Chuck Taylor',41,'White',100,'available',1),
-('Timberland','Classic Boot',42,'Brown',200,'available',3),
-('Timberland','Classic Boot',41,'Brown',200,'available',3),
-('Birkenstock','Arizona',38,'Brown',90,'available',4),
-('Birkenstock','Arizona',39,'Black',90,'available',4),
-('Vans','Old Skool',40,'Black',110,'available',1),
-('Vans','Old Skool',41,'White',110,'available',1),
-('New Balance','574',39,'Grey',140,'available',2),
-('New Balance','574',40,'Grey',140,'available',2),
-('Reebok','Classic',40,'White',130,'available',1),
-('Reebok','Classic',41,'White',130,'available',1),
-('Dr Martens','1460',42,'Black',210,'available',3),
-('Dr Martens','1460',41,'Black',210,'available',3);
+('Nike','Air Zoom Pegasus',40,'Black',150,'available',1),
+('Nike','Air Zoom Pegasus',41,'White',150,'available',1),
+
+('Adidas','Ultraboost 22',39,'Black',160,'available',2),
+('Adidas','Ultraboost 22',40,'White',160,'available',2),
+
+('ASICS','Gel Nimbus',40,'Blue',170,'available',3),
+('ASICS','Gel Nimbus',41,'Black',170,'available',3),
+
+('New Balance','Fresh Foam 1080',39,'Grey',165,'available',4),
+('New Balance','Fresh Foam 1080',40,'Grey',165,'available',4),
+
+('Puma','Deviate Nitro',40,'Black',150,'available',5),
+('Puma','Deviate Nitro',41,'White',150,'available',5),
+
+('Brooks','Ghost 15',39,'Blue',160,'available',6),
+('Brooks','Ghost 15',40,'Black',160,'available',6),
+
+('Saucony','Endorphin Speed',40,'Red',175,'available',7),
+('Saucony','Endorphin Speed',41,'Black',175,'available',7),
+
+('Hoka','Clifton 9',39,'White',180,'available',8),
+('Hoka','Clifton 9',40,'Blue',180,'available',8),
+
+('Nike','Alphafly',41,'Orange',200,'available',1),
+('Adidas','Adios Pro',42,'Green',200,'available',2),
+('ASICS','Metaspeed Sky',41,'Red',210,'available',3),
+('Hoka','Rocket X',42,'Black',210,'available',8);
---
+++ my-e-commerce/06_payments.sql
@@ -0,0 +1,10 @@
+INSERT INTO payments (rental_id, amount, payment_date, payment_method)
+VALUES
+(1,300,'2026-03-01','cash'),
+(2,240,'2026-03-02','promptpay'),
+(3,100,'2026-03-03','cash'),
+(4,400,'2026-03-04','credit_card'),
+(5,180,'2026-03-05','promptpay'),
+(6,140,'2026-03-06','cash'),
+(7,130,'2026-03-07','promptpay'),
+(8,420,'2026-03-08','credit_card');
